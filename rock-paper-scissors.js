c7b2845407d09db55594ae98d1994408ee84c370
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2024-11-30 21:57:04 -0500

    Added alert for when player enters invalid input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,21 @@
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0;
+
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+
+function playGame() {
+    rounds++;
+    console.log("Round " + rounds)
+    while (rounds <= 5) {
+        // const humanSelection = getHumanChoice();
+        // const computerSelection = getComputerChoice();
+        playRound(getHumanChoice(), getComputerChoice());
+
+    }
+}
 
 function playRound(humanChoice, computerChoice) {
     console.log("Your Choice: " + humanChoice);
@@ -7,30 +23,34 @@ function playRound(humanChoice, computerChoice) {
 
     if (humanChoice === computerChoice) {
         console.log("It's a tie!");
+        return "Tie";
     } else if (humanChoice === "Rock") {
         if (computerChoice === "Scissors") {
             console.log("You win! Rock crushes Scissors.")
+            return "Win";
         } else {
             console.log("You lose! Paper covers Rock.");
+            return "Lose";
         }
     } else if (humanChoice === "Paper") {
         if (computerChoice === "Rock") {
             console.log("You win! Paper covers Rock.")
+            return "Win";
         } else {
             console.log("You lose! Scissors cuts Paper.");
+            return "Lose";
         }
     } else if (humanChoice === "Scissors") {
         if (computerChoice === "Paper") {
             console.log("You win! Scissors cuts Paper.")
+            return "Win";
         } else {
             console.log("You lose! Rock crushes scissors.");
+            return "Lose";
         }
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
 
 function getComputerChoice() {
     const computerChoice = Math.random();
@@ -43,11 +63,20 @@ function getComputerChoice() {
 }
 
 function getHumanChoice (){
-    const humanChoice = prompt("Rock, Paper, or Scissors?");
-    if (humanChoice.toLowerCase() === "rock")
-        return "Rock";
-    else if (humanChoice.toLowerCase() === "paper")
-        return "Paper";
-    else 
-        return "Scissors";
+    let humanChoice = "";
+    do {
+        humanChoice = prompt("Rock, Paper, or Scissors?");
+        if (humanChoice.toLowerCase() === "rock") {
+            return "Rock";
+        }
+        else if (humanChoice.toLowerCase() === "paper") {
+            return "Paper";
+        }
+        else if (humanChoice.toLowerCase() === "scissors") {
+            return "Scissors";
+        }
+        else {
+            alert("Invalid input. Please enter Rock, Paper, or Scissors.");
+        }
+    } while (humanChoice.toLowerCase() != "rock" && humanChoice.toLowerCase() != "paper" && humanChoice.toLowerCase() != "scissors");
 }
